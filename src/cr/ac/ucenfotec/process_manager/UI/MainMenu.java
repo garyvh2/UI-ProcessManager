202dd1908e38a5674cafd78091f70df9ef1d4372
Gary Valverde Hampton <garyhampton.12@gmail.com>
2018-05-28 02:44:55 -0600

    feat(mongo): Add Example
    Initial Mongo Example

--- src/cr/ac/ucenfotec/process_manager/UI/MainMenu.java
+++ src/cr/ac/ucenfotec/process_manager/UI/MainMenu.java
@@ -1,10 +1,19 @@
 package cr.ac.ucenfotec.process_manager.UI;
 
+import cr.ac.ucenfotec.process_manager.Annotations.MongoAttr;
+import cr.ac.ucenfotec.process_manager.ArchitectureComponents.MongoConnection;
+import cr.ac.ucenfotec.process_manager.Classes.Usuario;
+
 public class MainMenu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Usuario newUser = new Usuario();
+		newUser.set_id("116810122");
+		newUser.set_fname("Gary");
+		newUser.set_lname("Valverde");
+		
+		MongoConnection.GetInstance().Insert(newUser);
 	}
 
 }
